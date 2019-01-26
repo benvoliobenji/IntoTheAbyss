5bf0afa8c8c597ee330b851cbd9fa6f462f0038d
meberle <meberle@iastate.edu>
2019-01-26 15:31:13 -0600

    exp2 serve html

--- Experiments/meberle/exp2/src/main/java/exp2/Data.java
+++ Experiments/meberle/exp2/src/main/java/exp2/Data.java
@@ -1,16 +1,22 @@
 package exp2;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.time.LocalTime;
 
-@RestController
+@Controller
 public class Data {
 
-    @RequestMapping("/")
-    public String testIndex() {
-        return "Hello";
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
+
 
     @RequestMapping("/time")
     public String getTime() {
