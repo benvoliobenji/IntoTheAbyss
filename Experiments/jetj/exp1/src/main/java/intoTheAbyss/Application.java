8d62c933b819b6e82980743c1c7b553eec14d138
Jet Jacobs <jijacobs@iastate.edu>
2019-01-27 13:46:07 -0600

    Fixing some extra files being included due to my .gitignore

--- Experiments/jetj/exp1/src/main/java/intoTheAbyss/Application.java
+++ Experiments/jetj/exp1/src/main/java/intoTheAbyss/Application.java
@@ -1,4 +1,4 @@
-package intoTheAbyss.JetFirstSpring;
+package intoTheAbyss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
