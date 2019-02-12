9dd65bf6d48d6e82db517d0d5e42fc0aa3fe1aa4
Mitchell Eberle <meberle@iastate.edu>
2019-02-12 10:57:23 -0600

    corridor generation

--- Backend/intoTheAbyss/src/level/Level.java
+++ Backend/intoTheAbyss/src/level/Level.java
@@ -13,42 +13,51 @@ public class Level {
 	private Random rand;
 	private ArrayList<Player> players;
 	private Tile[][] grid;
-	private int[][] rooms;
+	public int[][] rooms;
 	private int[] spawn;
 
 	public Level() {
-		grid = new Tile[mapWidth][mapHeight];
+		grid = new Tile[mapHeight][mapWidth];
 		rand = new Random();
+		generate();
 	}
 
 	public Level(Random random) {
-		grid = new Tile[mapWidth][mapHeight];
-		rand = random;
+	  grid = new Tile[mapHeight][mapWidth];
+    rand = random;
 
 	}
 
 	public Level(int seed) {
-		grid = new Tile[mapWidth][mapHeight];
-		rand = new Random(seed);
+	  grid = new Tile[mapHeight][mapWidth];
+    rand = new Random(seed);
 	}
 
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
 
-	public Object[][] getGrid() {
+	public Tile[][] getGrid() {
 		return grid;
 	}
 
 	private void generate() {
-		for (Object[] dim : grid)
-			for (Object obj : dim)
-				obj = new Wall();
+		for (Tile[] dim : grid)
+			for (Tile t : dim)
+				t = new Wall();
+		
+		for (int i = 0; i < grid.length; i ++) {
+		  for (int j = 0; j < grid[0].length; j++) {
+		    grid[i][j] = new Wall();
+		  }
+		}
 
 		int num_rooms = 0;
-
+		rooms = new int[8][4];
+		
 		while (num_rooms < 8) {
-			createRoom(num_rooms++);
+			createRoom(num_rooms);
+			num_rooms++;
 		}
 
 		createCorridors();
@@ -58,7 +67,6 @@ public class Level {
 	}
 
 	private void createRoom(int idx) {
-		rooms = new int[8][4];
 		/* generate height, width, then top left corner x,y */
 		int i,j;
 		int a,b,c,d;
@@ -66,11 +74,12 @@ public class Level {
 
 		do {
 			/* create entry in room array */
-			c = rooms[idx][2] = (rand.nextInt() % 5) + 3;
-			d = rooms[idx][3] = (rand.nextInt() % 15) + 5;
-			a = rooms[idx][0] = (rand.nextInt() % (mapHeight - c - 1)) + 1;
-			b = rooms[idx][1] = (rand.nextInt() % (mapWidth - d - 1)) + 1;
-
+			c = rooms[idx][2] = (rand.nextInt(Integer.MAX_VALUE) % 5) + 6;
+			d = rooms[idx][3] = (rand.nextInt(Integer.MAX_VALUE) % 15) + 10;
+			
+			a = rooms[idx][0] = (rand.nextInt(Integer.MAX_VALUE) % (mapHeight - c - 1)) + 1;
+      b = rooms[idx][1] = (rand.nextInt(Integer.MAX_VALUE) % (mapWidth - d - 1)) + 1;
+      
 			/* check that the room's position is valid */
 			x = false;
 			for (i = 0; i < c+2; i++){
@@ -96,7 +105,7 @@ public class Level {
 		int cx = rooms[idx][1] + rooms[idx][3] / 2;
 		int cy = rooms[idx][0] + rooms[idx][2] / 2;
 
-		for (int i = 0; i < max; i++) {
+		for (int i = 0; i < rooms.length; i++) {
 			if (i == idx)
 				continue;
 			x = rooms[i][1] + rooms[i][3] / 2;
@@ -106,29 +115,53 @@ public class Level {
 	}
 
 	private void drawCorridor(int x1, int y1, int x2, int y2){		 
-		int leftRoom;
+		boolean leftRoom = false;
+		int xsign = x1 > x2 ? -1 : 1;
+		int ysign = y1 > y2 ? -1 : 1;
+    int i = 0;
+		
+	  while (x1 + i != x2) {
+	    if (grid[y1][x1 + i].getType() == 2)
+	      leftRoom = true;
+	    if (leftRoom && grid[y1][x1 + i].getType() != 2)
+        return;
+	    if (grid[y1][x1 + i].getType() == 2)
+	      grid[y1][x1 + i] = new Floor();
+	    if (leftRoom && (grid[y1 + 1][x1 + i].getType() != 2 || grid[y1 - 1][x1 + i].getType() != 2 ))
+	      return;
+	    i += xsign;
+	  }
+	  i = 0;
+	  
+	  while (y1 + i != y2) {
+	    if (grid[y1 + i][x2].getType() == 2)
+	      grid[y1 + i][x2] = new Floor();
+	    i += ysign;
+	  }
+	  
+	  /*boolean leftRoom;
 		int i;
 		int xsign = (x1 - x2) / Math.abs(x1 - x2);
 		int ysign = (y1 - y2) / Math.abs(y1 - y2);
 
-		leftRoom = 0;
+		leftRoom = false;
 		for (i = x1; xsign * i >= xsign * x2; i -= xsign) {
 			if (grid[y1][i].getType() == 2)
-				leftRoom = 1;
+				leftRoom = true;
 			if (leftRoom && grid[y1][i].getType() != 2)
 				return;
 			if (grid[y1][i].getType() == 2) {
 				grid[y1][i] = new Floor();
 			}
-			if (leftRoom && (grid[y1 + 1][i].getType() != 2 || grid[y1 - 1][i].getType() != 2))
-				return;
+			//if (leftRoom && (grid[y1 + 1][i].getType() != 2 || grid[y1 - 1][i].getType() != 2))
+			//	return;
 		}
 
 		for (i = y1; ysign * i >= ysign * y2; i -= ysign) {
 			if (grid[i][x2].getType() == 2) {
 				grid[i][x2] = new Floor();
 			}
-		}
+		}*/
 
 	}
 
@@ -140,10 +173,10 @@ public class Level {
     double dist = 100;
     double curr_dist;
 
-    for (i = 0; i < MAX_ROOMS; i++){
+    for (i = 0; i < rooms.length; i++){
         x = rooms[i][1] + rooms[i][3] / 2;
         y = rooms[i][0] + rooms[i][2] / 2;
-        curr_dist = sqrt((cX-x)*(cX-x) + (cY-y)*(cY-y));
+        curr_dist = Math.sqrt((cX-x)*(cX-x) + (cY-y)*(cY-y));
         if (curr_dist < dist){
             dist = curr_dist;
             idx = i;
