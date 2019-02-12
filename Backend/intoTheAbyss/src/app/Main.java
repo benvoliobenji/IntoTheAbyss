ccce4528053605f84a5e79ee1b01f2a6798d59ee
Mitchell Eberle <meberle@iastate.edu>
2019-02-12 12:38:39 -0600

    spring setup for backend

--- Backend/intoTheAbyss/src/app/Main.java
+++ Backend/intoTheAbyss/src/app/Main.java
@@ -4,13 +4,13 @@ public class Main {
 	public static int portTCP = 44444;
 	public static int portUDP = 44445;
 	public static void main(String[] args) {
-		World world = new World();
+		/*World world = new World();
 		
 		Server server = new Server();
 	    server.start();
 	    server.bind(portTCP, portUDP);
 	    
-	    /*server.addListener(new Listener() {
+	    server.addListener(new Listener() {
 	        public void received (Connection connection, Object object) {
 	           if (object instanceof SomeRequest) {
 	              SomeRequest request = (SomeRequest)object;
