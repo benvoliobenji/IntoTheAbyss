4e572b9c43e34b12adf57348ec5206cc095f2da7
Joe <joevt@iastate.edu>
2019-02-14 18:46:49 -0600

    Forgot to add changed files from my last commit.

---
+++ Frontend/IntotheAbyss/app/src/main/java/com/example/intotheabyss/activities/game/GameView2.kt
@@ -0,0 +1,63 @@
+package com.example.intotheabyss.activities.activities.game
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import com.example.intotheabyss.activities.game.GameThread2
+
+
+class GameView2(context: Context, attributes: AttributeSet) : SurfaceView(context, attributes), SurfaceHolder.Callback {
+
+    private val thread: GameThread2
+
+    init {
+
+        // Don't really know what this does
+        holder.addCallback(this)
+
+        // This instantiates the game thread when we start the game
+        thread = GameThread2(holder, this)
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder?) {
+        //When surface is "destroyed", stop the thread
+        var retry = true
+        while (retry) {
+            try {
+                thread.setRunning(false)
+                thread.join()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            retry = false
+        }
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder?) {
+        //Start the game thread
+        thread.setRunning(true)
+        thread.start()
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+        //TODO: ??? Don't really know what to do here, yet!
+    }
+
+    /**
+     * This is where we will update game variables
+     */
+    fun update() {
+
+    }
+
+    /**
+     * This is where we will draw things onto the game "canvas"
+     */
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+    }
+
+}
