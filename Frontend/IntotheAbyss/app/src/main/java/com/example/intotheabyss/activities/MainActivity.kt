be702c0b75c142dde97c0d3703415eb2ac74d321
joevt <joevt@iastate.edu>
2019-02-16 16:08:05 -0600

    Trying to fix merge conflict

--- Frontend/IntotheAbyss/app/src/main/java/com/example/intotheabyss/activities/MainActivity.kt
+++ Frontend/IntotheAbyss/app/src/main/java/com/example/intotheabyss/activities/MainActivity.kt
@@ -4,35 +4,36 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.StrictMode
 import android.widget.Button
 import com.example.intotheabyss.R
 import com.example.intotheabyss.networking.Network
+import com.example.intotheabyss.networking.NetworkRunnable
 
 class MainActivity : AppCompatActivity() {
 
-    private var network: Network = Network()
+    private var networkThread = Thread()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ring = MediaPlayer.create(this, R.raw.car_sound)
 
         val play = findViewById<Button>(R.id.playButton)
         play.setOnClickListener {
             val intent = Intent(this, DungeonActivity::class.java)
             startActivity(intent)
+            ring.start()
 
-            //Proposed fix (For network errors maybe?)
-            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-            StrictMode.setThreadPolicy(policy)
-
-//            this.network.connect()
+            if(!networkThread.isAlive) {
+                networkThread = Thread(NetworkRunnable())
+                networkThread.start()
+            }
         }
 
         val settings = findViewById<Button>(R.id.settingsButton)
         settings.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
-                startActivity(intent)
+            startActivity(intent)
         }
 
 
