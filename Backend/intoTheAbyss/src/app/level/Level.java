953fa2036e4170fc3549574c006f2cc7cb63304d
Jet Jacobs <jijacobs@iastate.edu>
2019-03-05 09:33:08 -0600

    Messed up with merge resolution

---
+++ Backend/intoTheAbyss/src/app/level/Level.java
@@ -0,0 +1,201 @@
+package app.level;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Random;
+
+import app.player.Player;
+import app.tiles.*;
+import app.utils.TileTypes;
+
+public class Level {
+	public static final int mapWidth = 100;
+	public static final int mapHeight = 25;
+
+	private Random rand;
+	private ArrayList<Player> players;
+	private Tile[][] grid;
+	private Room[] rooms;
+	private Point spawn;
+	private Point stair;
+
+	public Level() {
+		grid = new Tile[mapHeight][mapWidth];
+		rand = new Random();
+		generate();
+	}
+
+	public Level(Random random) {
+		grid = new Tile[mapHeight][mapWidth];
+		rand = random;
+		generate();
+	}
+
+	public Level(int seed) {
+		grid = new Tile[mapHeight][mapWidth];
+		rand = new Random(seed);
+		generate();
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+
+	public Tile[][] getGrid() {
+		return grid;
+	}
+
+	public Point getSpawn() {
+		return spawn;
+	}
+
+	public Point getStair() {
+		return stair;
+	}
+
+	private void generate() {
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				grid[i][j] = new Wall();
+			}
+		}
+
+		int num_rooms = 0;
+		rooms = new Room[8];
+
+		while (num_rooms < 8) {
+			createRoom(num_rooms);
+			num_rooms++;
+		}
+
+		createCorridors();
+		createSpawn();
+		createStair();
+
+	}
+
+	private void createRoom(int idx) {
+		/* generate height, width, then top left corner x,y */
+		int i, j;
+		int x, y;
+		boolean loop;
+
+		do {
+			/* create entry in room array */
+			//y length and x length
+			//y corner x corner
+			rooms[idx] = new Room(rand);
+
+
+			/* check that the room's position is valid */
+			loop = false;
+			for (i = 0; i < rooms[idx].getyLength() + 2; i++) {
+				for (j = 0; j < rooms[idx].getxLength() + 2; j++) {
+					x = rooms[idx].getCorner().x;
+					y = rooms[idx].getCorner().y;
+					if (grid[y + i - 1][x + j - 1].getType() != TileTypes.WALL) // add an enum or smth for tile types?
+						loop = true;
+				}
+			}
+			/* if not, retry */
+		} while (loop);
+
+		/* write array into grid */
+		for (i = 0; i < rooms[idx].getyLength(); i++) {
+			for (j = 0; j < rooms[idx].getxLength(); j++) {
+				x = rooms[idx].getCorner().x;
+				y = rooms[idx].getCorner().y;
+				grid[y + i][x + j] = new Floor();
+			}
+		}
+	}
+
+	private void createCorridors() {
+		int idx = findCenterRoom();
+		Point center, p;
+		center = rooms[idx].getCenter();
+
+		for (int i = 0; i < rooms.length; i++) {
+			if (i == idx)
+				continue;
+			p = rooms[i].getCenter();
+			drawCorridor(p.x, p.y, center.x, center.y);
+		}
+	}
+
+	private void drawCorridor(int x1, int y1, int x2, int y2) {
+		boolean leftRoom = false;
+		int xsign = x1 > x2 ? -1 : 1;
+		int ysign = y1 > y2 ? -1 : 1;
+		int i = 0;
+
+		while (x1 + i != x2) {
+			if (grid[y1][x1 + i].getType() == TileTypes.WALL)
+				leftRoom = true;
+			if (leftRoom && grid[y1][x1 + i].getType() != TileTypes.WALL)
+				return;
+			if (grid[y1][x1 + i].getType() == TileTypes.WALL)
+				grid[y1][x1 + i] = new Floor();
+			if (leftRoom && (grid[y1 + 1][x1 + i].getType() != TileTypes.WALL ||
+					grid[y1 - 1][x1 + i].getType() != TileTypes.WALL))
+				return;
+			i += xsign;
+		}
+		i = 0;
+
+		while (y1 + i != y2) {
+			if (grid[y1 + i][x2].getType() == TileTypes.WALL)
+				grid[y1 + i][x2] = new Floor();
+			i += ysign;
+		}
+	}
+
+	private int findCenterRoom() {
+		int cX = 40;
+		int cY = 21 / 2;
+		int i;
+		Point p;
+		int idx = 0;
+		double dist = 1000;
+		double curr_dist;
+
+		for (i = 0; i < rooms.length; i++) {
+			p = rooms[i].getCenter();
+			curr_dist = Math.sqrt((cX - p.x) * (cX - p.x) + 4 * (cY - p.y) * (cY - p.y));
+			if (curr_dist < dist) {
+				dist = curr_dist;
+				idx = i;
+			}
+		}
+
+		return idx;
+	}
+
+	private void createStair() {
+		int idx = findCenterRoom();
+		int r = 0;
+		while (r == idx)
+			r = rand.nextInt(rooms.length);
+		stair = rooms[r].getRandomPointInRoom();
+		grid[stair.y][stair.x] = new Stair();
+	}
+
+	private void createSpawn() {
+		int idx = findCenterRoom();
+		spawn = rooms[idx].getCenter();
+	}
+
+	public void fillGridForDefaultMap() {
+		for(int i=0; i < mapWidth; i++) {
+			for(int j=0; j < mapHeight; j++) {
+				//Checks if the selected index is an edge of the grid
+				if(i == 0 || j == 0 || i == mapWidth-1 || j == mapHeight-1) {
+					grid[i][j]  = new Wall();
+				}else {
+					grid[i][j] = new Floor();
+				}
+			}
+		}
+	}
+
+}
---
+++ Backend/intoTheAbyss/src/app/level/Room.java
@@ -0,0 +1,56 @@
+package app.level;
+
+import java.awt.Point;
+import java.util.Random;
+
+public class Room {
+
+	private Point corner;
+	private int xLength, yLength;
+	Random rand;
+
+	public Room(Random rand) {
+		corner = new Point();
+		this.rand = rand;
+
+		/* create entry in room array */
+		// y length and x length
+		yLength = rand.nextInt(4) + 4;
+		xLength = rand.nextInt(15) + 10;
+		// y corner x corner
+		corner.y = rand.nextInt(Level.mapHeight - yLength - 1) + 1;
+		corner.x = rand.nextInt(Level.mapWidth - xLength - 1)  + 1;
+	}
+
+	public Point getCorner() {
+		return corner;
+	}
+
+	public Point getCenter() {
+		Point p = new Point();
+		p.x = corner.x + xLength / 2;
+		p.y = corner.y + yLength / 2;
+		return p;
+	}
+
+	public int getxLength() {
+		return xLength;
+	}
+
+	public int getyLength() {
+		return yLength;
+	}
+
+	public boolean overlapsRoom(Room differentRoom) {
+		return false;
+	}
+
+	public Point getRandomPointInRoom() {
+		Point p = new Point();
+		p.x = corner.x + 1 + rand.nextInt(xLength - 2);
+		p.y = corner.y + 1 + rand.nextInt(yLength - 2);
+		return p;
+	}
+
+
+}
