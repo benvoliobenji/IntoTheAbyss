479313f97a6dd68945c6248ba77159ceadefe97f
Jet Jacobs <jijacobs@iastate.edu>
2019-04-20 07:10:46 -0500

    Fixed the issues with testing

---
+++ Backend/intoTheAbyss/tests/tests/SimpleTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class SimpleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
