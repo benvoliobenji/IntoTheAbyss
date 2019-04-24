86145c64021e6ec262ac96fdee0a22a30cff369d
Benjamin Vogel <bavogel@iastate.edu>
2019-04-24 14:37:02 -0500

    Wrapped up API Documentation on Client Side

--- Frontend/IntotheAbyss/app/src/main/java/com/example/intotheabyss/networking/updateverification/UpdateVerificationType.kt
+++ Frontend/IntotheAbyss/app/src/main/java/com/example/intotheabyss/networking/updateverification/UpdateVerificationType.kt
@@ -1,5 +1,9 @@
 package com.example.intotheabyss.networking.updateverification
 
+/**
+ * An enum class designed to clarify the return statement of UpdateVerification.
+ * @author Benjamin Vogel
+ */
 enum class UpdateVerificationType {
     NONE,
     POSITION,
