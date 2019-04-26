96e2004a416e20bcce0072f6e0bcb98018f7151a
Benjamin Vogel <bavogel@iastate.edu>
2019-04-26 16:21:59 -0500

    Support Entity Changes to Monster

--- Frontend/IntotheAbyss/app/src/main/java/com/example/intotheabyss/game/entity/monster/Monster.kt
+++ Frontend/IntotheAbyss/app/src/main/java/com/example/intotheabyss/game/entity/monster/Monster.kt
@@ -1,6 +1,7 @@
 package com.example.intotheabyss.game.entity.monster
 
 import com.example.intotheabyss.game.entity.Entity
+import com.example.intotheabyss.game.entity.EntityType
 
 /**
  * A Monster Class. Inherits from MonsterInterface and will be a subclass of Entity (still in development). Logic and
@@ -14,6 +15,6 @@ class Monster : MonsterInterface, Entity {
      * A constructor to create a Monster from an Entity
      * @param e An Entity to cast to Monster.
      */
-    constructor(e: Entity) : super(e.ID, e.x, e.y, e.floor, e.health)
+    constructor(e: Entity) : super(e.ID, e.x, e.y, e.floor, e.health, 0, EntityType.MONSTER)
 
 }
